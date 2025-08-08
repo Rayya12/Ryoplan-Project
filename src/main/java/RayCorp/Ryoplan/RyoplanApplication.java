a23aab9660b469ae89a99445fe7335225a2a4505
Rayya12 <rayaalului@gmail.com>
2025-08-08 07:41:54 +0700

    Initial commit Ryoplan

---
+++ src/main/java/RayCorp/Ryoplan/RyoplanApplication.java
@@ -0,0 +1,13 @@
+package RayCorp.Ryoplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyoplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyoplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RayCorp/Ryoplan/RyoplanApplicationTests.java
@@ -0,0 +1,13 @@
+package RayCorp.Ryoplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyoplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
